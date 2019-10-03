db0d78165fa245b04e74d43a1dceefe7a77eabfc
girishma18 <v.girishma@gmail.com>
2019-10-03 18:51:08 +0530

    project on springs and hibernate

---
+++ onlineshopping/src/main/java/net/giri/onlineshopping/controller/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author KhozemaNullwala
+ *
+ */
+package net.giri.onlineshopping.controller;
---
+++ onlineshopping/src/main/java/net/giri/onlineshopping/model/CheckoutModel.java
@@ -0,0 +1,75 @@
+package net.giri.onlineshopping.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+import net.giri.shoppingbackend.dto.Address;
+import net.giri.shoppingbackend.dto.Cart;
+import net.giri.shoppingbackend.dto.CartLine;
+import net.giri.shoppingbackend.dto.OrderDetail;
+import net.giri.shoppingbackend.dto.User;
+
+public class CheckoutModel implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private User user;
+	private Address shipping;
+	private Cart cart;
+	private List<CartLine> cartLines;
+	private OrderDetail orderDetail;
+	private double checkoutTotal;
+
+	public OrderDetail getOrderDetail() {
+		return orderDetail;
+	}
+
+	public void setOrderDetail(OrderDetail orderDetail) {
+		this.orderDetail = orderDetail;
+	}
+
+	public Cart getCart() {
+		return cart;
+	}
+
+	public void setCart(Cart cart) {
+		this.cart = cart;
+	}
+
+	public double getCheckoutTotal() {
+		return checkoutTotal;
+	}
+
+	public void setCheckoutTotal(double checkoutTotal) {
+		this.checkoutTotal = checkoutTotal;
+	}
+
+	public List<CartLine> getCartLines() {
+		return cartLines;
+	}
+
+	public void setCartLines(List<CartLine> cartLines) {
+		this.cartLines = cartLines;
+	}
+
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Address getShipping() {
+		return shipping;
+	}
+
+	public void setShipping(Address shipping) {
+		this.shipping = shipping;
+	}
+	
+}
